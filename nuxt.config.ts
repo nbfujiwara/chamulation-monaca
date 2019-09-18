a08c6021d7d2ff66e65c6dbed163c54ad17c56d5
yuji.fujiwara <yuji.fujiwara@nijibox.co.jp>
2019-09-18 18:11:21 +0900

    初期セット

---
+++ nuxt.config.ts
@@ -0,0 +1,50 @@
+import colors from 'vuetify/es5/util/colors'
+import { Configuration } from '@nuxt/types'
+
+const nuxtConfig: Configuration = {
+  mode: 'spa',
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  loading: { color: '#fff' },
+  css: [],
+  plugins: [],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
+  modules: [],
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      light: true,
+      themes: {
+        light: {
+          primary: '#3f51b5',
+          secondary: '#b0bec5',
+          accent: '#8c9eff',
+          error: '#b71c1c'
+        },
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  }
+}
+module.exports = nuxtConfig
